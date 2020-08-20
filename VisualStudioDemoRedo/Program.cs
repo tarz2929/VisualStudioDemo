ced7dd7be401950ae2b1054a1ba0a8fb03e64829
James Grieve <jamesonrgrieve@gmail.com>
2020-08-20 13:54:58 -0600

    Practice spec;

--- VisualStudioDemoRedo/Program.cs
+++ VisualStudioDemoRedo/Program.cs
@@ -7,38 +7,11 @@ namespace VisualStudioDemoRedo
     {
         static void Main(string[] args)
         {
-            int[] arrayOfNumbers = { 9, 88, 40, 44, 78, 26, 1, 90 };
-            List<int> oddNumbers = new List<int>();
-            List<int> evenNumbers = new List<int>();
-
-            // For each of the numbers in our array of numbers
-            for (int i = 0; i < arrayOfNumbers.Length; i++)
-            {
-                // If the number is even (remainder of division by 2 is 0)
-                if (arrayOfNumbers[i] % 2 == 0)
-                {
-                    // Add it to the even array and increase our count of even numbers
-                    evenNumbers.Add(arrayOfNumbers[i]);
-                }
-                // If it's not even, it's odd
-                else
-                {
-                    // Add it to the odd array and increase our count of odd numbers
-                    oddNumbers.Add(arrayOfNumbers[i]);
-                }
-            }
-
-            Console.WriteLine("Evens:");
-            foreach (int number in evenNumbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            Console.WriteLine("Odds:");
-            foreach (int number in oddNumbers)
-            {
-                Console.WriteLine(number);
-            }
+            /* Practice Spec:
+             * Create a program that will generate a 2x2 2 dimmensional array, and will repeatedly display the current layout
+             * then prompt the user for a row number, column number, and value until the user enters "exit". 
+             * When the user enters the info, overwrite that position with the value the user provided.
+             */
         }
 
 
