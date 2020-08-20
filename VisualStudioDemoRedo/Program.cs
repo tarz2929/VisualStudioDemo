d051b6d1cb1b898fdf59299ca2b84499e5bc3c18
James Grieve <jamesonrgrieve@gmail.com>
2020-08-20 13:29:09 -0600

    Array copying vs referencing;

--- VisualStudioDemoRedo/Program.cs
+++ VisualStudioDemoRedo/Program.cs
@@ -6,40 +6,13 @@ namespace VisualStudioDemoRedo
     {
         static void Main(string[] args)
         {
-            int[] arrayOfNumbers = { 9, 88, 40, 44, 78, 26, 1, 90 };
-            int[] oddNumbers = new int[8], evenNumbers = new int[8];
-            int oddCount = 0, evenCount = 0;
+            int[] myArrayOne = { 42, 12, 7, 33, 5, 3 };
+            // Clone() will create a copy of the array, as opposed to returning the memory address of the original.
+            int[] myArrayTwo = (int[])myArrayOne.Clone();
+            //int[] myArrayTwo = myArrayOne;
 
-            // For each of the numbers in our array of numbers
-            for (int i = 0; i < arrayOfNumbers.Length; i++)
-            { 
-                // If the number is even (remainder of division by 2 is 0)
-                if (arrayOfNumbers[i] % 2 == 0)
-                {
-                    // Add it to the even array and increase our count of even numbers
-                    evenNumbers[evenCount] = arrayOfNumbers[i];
-                    evenCount++;
-                }
-                // If it's not even, it's odd
-                else
-                {
-                    // Add it to the odd array and increase our count of odd numbers
-                    oddNumbers[oddCount] = arrayOfNumbers[i];
-                    oddCount++;
-                }
-            }
+            myArrayTwo[3] = 100;
 
-            Console.WriteLine("Evens:");
-            foreach(int number in evenNumbers)
-            {
-                Console.WriteLine(number);
-            }
-
-            Console.WriteLine("Odds:");
-            foreach (int number in oddNumbers)
-            {
-                Console.WriteLine(number);
-            }
 
         }
 
