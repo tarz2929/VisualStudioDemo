ebc8995f03031b1aabe0aec186434843b029529e
James Grieve <jamesonrgrieve@gmail.com>
2020-08-20 15:31:02 -0600

    Object examples;

---
+++ VisualStudioDemoRedo/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisualStudioDemoRedo
+{
+    public class Person
+    {
+        private int _age;
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get {
+                return _age;
+            }
+            set {
+                if (value > 1000)
+                {
+                    throw new Exception("You can't be that old!");
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public string Greeting()
+        {
+            return $"Hello! My name is {Name}, I'm {Age} years old and full of energy! Nice to meet you!";
+        }
+
+        public Person()
+        {
+            Name = "John Doe";
+            Age = 18;
+        }
+    }
+}
--- VisualStudioDemoRedo/Program.cs
+++ VisualStudioDemoRedo/Program.cs
@@ -4,78 +4,25 @@ using System.Collections.Generic;
 namespace VisualStudioDemoRedo
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            /* Practice Spec:
-             * Create a program that will generate a 2x2 2 dimmensional array, and will repeatedly display the current layout
-             * then prompt the user for a row number, column number, and value until the user enters "exit". 
-             * When the user enters the info, overwrite that position with the value the user provided.
-             */
+            Person bob = new Person();
+            bob.Name = "Bob";
+            bob.Age = 50;
 
-            int[,] theArray = new int[2, 2];
-
-            string userInput = "";
-
-            do
+            Person joe = new Person
             {
-                DisplayArray(theArray);
+                Name = "Joe",
+                Age = 25
+            };
 
-                Console.Write("Please enter \"Exit\" to quit, or anything else to continue: ");
-                userInput = Console.ReadLine();
+            Person defaultPerson = new Person();
 
-                if (userInput.ToLower() != "exit")
-                {
-                    int row, column, value;
+            Console.WriteLine(bob.Greeting());
+            Console.WriteLine(joe.Greeting());
+            Console.WriteLine(defaultPerson.Greeting());
 
-                    row = GetValidInt("Please enter a row number: ", 0, 2);
-
-                    column = GetValidInt("Please enter a column number: ", 0, 2);
-
-                    value = GetValidInt("Please enter a value: ", int.MinValue, int.MaxValue);
-
-                    theArray[column, row] = value;
-                }
-            } while (userInput.ToLower() != "exit");
-        }
-
-        static void DisplayArray(int[,] arrayToDisplay)
-        {
-            Console.WriteLine("-----");
-            for (int r = 0; r < 2; r++)
-            {
-                Console.Write("|");
-                for (int c = 0; c < 2; c++)
-                {
-                    Console.Write(arrayToDisplay[c, r] + "|");
-                }
-                Console.WriteLine("\n-----");
-            }
-        }
-
-        static int GetValidInt(string prompt, int min, int max)
-        {
-            bool valid = false;
-            int userInput = -1;
-            do
-            {
-                Console.Write(prompt);
-                try
-                {
-                    userInput = int.Parse(Console.ReadLine());
-                    if (userInput < min || userInput > max)
-                    {
-                        throw new Exception("Input was outside of the bounds of the array");
-                    }
-                    valid = true;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("ERROR: " + ex.Message);
-                    Console.WriteLine("Please try again.");
-                }
-            } while (!valid);
-            return userInput;
         }
     }
 }
