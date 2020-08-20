27e304db85458d1b8c407fd1b411434548deafcd
James Grieve <jamesonrgrieve@gmail.com>
2020-08-20 13:46:01 -0600

    List example;

--- VisualStudioDemoRedo/Program.cs
+++ VisualStudioDemoRedo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VisualStudioDemoRedo
 {
@@ -6,16 +7,40 @@ namespace VisualStudioDemoRedo
     {
         static void Main(string[] args)
         {
-            int[] myArrayOne = { 42, 12, 7, 33, 5, 3 };
+            int[] arrayOfNumbers = { 9, 88, 40, 44, 78, 26, 1, 90 };
+            List<int> oddNumbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
 
-            // As memory address references, the original array is passed into the method and can be modified therein.
-            ModifyArray(myArrayOne);
+            // For each of the numbers in our array of numbers
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                // If the number is even (remainder of division by 2 is 0)
+                if (arrayOfNumbers[i] % 2 == 0)
+                {
+                    // Add it to the even array and increase our count of even numbers
+                    evenNumbers.Add(arrayOfNumbers[i]);
+                }
+                // If it's not even, it's odd
+                else
+                {
+                    // Add it to the odd array and increase our count of odd numbers
+                    oddNumbers.Add(arrayOfNumbers[i]);
+                }
+            }
 
+            Console.WriteLine("Evens:");
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Odds:");
+            foreach (int number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
         }
-        static void ModifyArray(int[] theArray)
-        {
-            theArray[2] = 5;
-        }
+
 
 
     }
