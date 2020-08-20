bb3ebd99f047c1ebe03b5258d74a095503a810b4
James Grieve <jamesonrgrieve@gmail.com>
2020-08-20 11:10:24 -0600

    Initial commit;

---
+++ VisualStudioDemoRedo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioDemoRedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
